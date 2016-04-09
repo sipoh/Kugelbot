2712d0c92cbdd4fde11c234f70d3aca8d6f8f8d1
sipoh <webmaster@simon-pohlmann.de>
2016-04-09 21:02:42 +0200

    ADD: irc

--- messagehandler/onText.js
+++ messagehandler/onText.js
@@ -1,15 +1,12 @@
-var merke = false,
-    bot;
-
-var feed = require('feed-read'),  // require the feed-read module
+var bot,
+    feed = require('feed-read'),  // require the feed-read module
+    moment = require('moment'),
     url = ["https://www.tagesschau.de/xml/rss2", 'www.tagesschau.de']; // RSS Feed
 
 var ausgabe = function (req, msg, ausgabe_text) {
     console.log(req);
     // fetch rss feed for the url:
     feed(req[0], function (err, articles) {
-        //err = 'Not defined';
-
         //Set amount of articles to be listed.
         if (articles.length > 5) {
             var max = 5;
@@ -17,27 +14,20 @@ var ausgabe = function (req, msg, ausgabe_text) {
         else {
             var max = articles.length;
         }
-
         // loop through the list of articles returned
         for (var i = 0; i < max; i++) {
-
             // stream article title (and what ever else you want) to client
-            console.log(articles[i]);
+            //console.log(articles[i]);
             //ausgabe_text.push(articles[i].title);
-
             //bot.sendMessage(msg.chat.id, articles[i].title);
             ausgabe_text = ausgabe_text + '[' + articles[i].title + '](' + articles[i].link + ')\n';
-            //console.log(ausgabe_text);
-
-            // check we have reached the end of our list of articles & urls
+            console.log(ausgabe_text);
         } //  end inner for loop
         //ausgabe_text = ausgabe_text + '\n' + req[1];
         options = {parse_mode: 'Markdown'};
         bot.sendMessage(msg.chat.id, ausgabe_text, options);
     }); // end call to feed (feed-read) method
-
 };
-
 
 var callback = function (msg) {
     console.log(msg);
@@ -69,30 +59,28 @@ var callback = function (msg) {
             ausgabe(url, msg, ausgabe_text);
             break;
         case '1337':
-            antwort = '1337';
-            option = {reply_to_message_id: msg.message_id};
+            var msgTime = moment.unix(msg.date),
+                time = parseInt(msgTime.format('HHmm'));
+
+            if (time === 1337){        // 1337
+                antwort = '1337';
+            } else if (time < 1337) {  // zu früh
+                antwort = 'Zu früh.';
+            } else {                   // zu spät
+                antwort = 'Zu spät.';
+            }
+
+            options = {reply_to_message_id: msg.message_id};
             bot.sendMessage(msg.chat.id, antwort, options);
             break;
         case 'gute nacht':
-        case 'good n8':
+        case 'gute nacht!':
         case 'gute n8':
+        case 'good night':
+        case 'good night!':
+        case 'good n8':
             bot.sendSticker(msg.chat.id, 'BQADAwADrQIAAqbJWAABfU1XyeWYp6gC');
             break;
-        //case 'ja':
-        //case 'jop':
-        //case 'jo':
-        //case 'jap':
-        //case 'genau':
-        //    if (merke) {
-        //        antwort = 'Okay';
-        //        options = {parse_mode: 'Markdown', reply_to_message_id: msg.message_id};
-        //        bot.sendMessage(msg.chat.id, antwort, options);
-        //        merke = false;
-        //        break;
-        //    };
-        default:
-            //antwort = 'Hast du gerade ' + text + ' gesagt?';
-            merke = true;
     }
 };
 
